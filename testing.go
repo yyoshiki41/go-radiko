df06192e809f585df6407f11ea57981b75a06484
yyoshiki41 <yyoshiki41@gmail.com>
2016-11-04 17:46:45 +0900

    Fixed testing

--- testing.go
+++ testing.go
@@ -9,7 +9,7 @@ import (
 func createTestTempDir(t *testing.T) (string, func()) {
 	dir, err := ioutil.TempDir("", "test-go-radiko")
 	if err != nil {
-		t.Fatalf("Failed to create temp dir: ", err)
+		t.Fatalf("Failed to create temp dir: %s", err)
 	}
 
 	return dir, func() { os.RemoveAll(dir) }
